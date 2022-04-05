5988e8f1885f0396c4e914e656c587020b540868
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-04-05 09:36:00 +0200

    Created initial project setup and .gitignore

---
+++ src/main/java/com/productdock/rbclibraryrental/RbcLibraryRentalApplication.java
@@ -0,0 +1,13 @@
+package com.productdock.rbclibraryrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbcLibraryRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbcLibraryRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productdock/rbclibraryrental/RbcLibraryRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productdock.rbclibraryrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbcLibraryRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
