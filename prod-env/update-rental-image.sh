9ee73ab19e38c7521e0f22b46ae84e850f29c748
NenadBecanovicPD <100684660+NenadBecanovicPD@users.noreply.github.com>
2022-05-19 11:03:05 +0200

    Update prod-env script

--- prod-env/update-rental-image.sh
+++ prod-env/update-rental-image.sh
@@ -6,5 +6,5 @@ docker stop rbc-library-rental
 docker rm rbc-library-rental
 docker rmi $(docker images | grep "rbc-library-rental")
 
-docker run -dp 8083:8080 --env-file /home/pd-library/.rental-service_env --name=rbc-library-rental gcr.io/prod-pd-library/rbc-library-rental:$1
+docker run -dp 8083:8080 -e KAFKA_SERVER_URL=$KAFKA_SERVER_URL --env-file /home/pd-library/.rental-service_env --name=rbc-library-rental gcr.io/prod-pd-library/rbc-library-rental:$1
 docker container ls -a
