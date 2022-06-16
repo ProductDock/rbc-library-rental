03ad3ff051ff0d3bd645885e0993900c67fe957e
Dusan <dusan.petrovic@productdock.com>
2022-06-16 14:06:03 +0200

    Refactored scheduler to execute at fixed rate

--- src/main/java/com/productdock/library/rental/RentalApplication.java
+++ src/main/java/com/productdock/library/rental/RentalApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafka
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableMongoRepositories
 @EnableMongock
+@EnableScheduling
 public class RentalApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/productdock/library/rental/scheduler/SchedulerRule.java
+++
@@ -1,6 +0,0 @@
-package com.productdock.library.rental.scheduler;
-
-public enum SchedulerRule {
-    WORKDAYS,
-    WEEKDAYS,
-}
