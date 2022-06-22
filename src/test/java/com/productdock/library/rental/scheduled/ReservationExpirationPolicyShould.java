b13ceab3b0244acec51693c133be1836a8125d8a
Dusan <dusan.petrovic@productdock.com>
2022-06-22 10:18:34 +0200

    Refactor to parametrized tests

--- src/test/java/com/productdock/library/rental/scheduled/ReservationExpirationPolicyShould.java
+++ src/test/java/com/productdock/library/rental/scheduled/ReservationExpirationPolicyShould.java
@@ -2,10 +2,15 @@ package com.productdock.library.rental.scheduled;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -16,7 +21,7 @@ class ReservationExpirationPolicyShould {
 
     private static final DateProvider dateProvider = mock(DateProvider.class);
 
-    private static final int limit = 4;
+    private static final int TIME_LIMIT = 4;
 
     private static final Date DATE_13_06_2022 = new Date(1655107200000L);
     private static final Date DATE_15_06_2022 = new Date(1655287200000L);
@@ -24,60 +29,31 @@ class ReservationExpirationPolicyShould {
     private static final Date DATE_20_06_2022 = new Date(1655719200000L);
     private static final Date DATE_21_06_2022 = new Date(1655805600000L);
 
-    @Test
-    void checkIfReservationIsExpiredWhenWeekdaysPolicyAndCurrentTimeBeforeExpiration() {
-        given(dateProvider.now()).willReturn(DATE_15_06_2022);
+    @ParameterizedTest
+    @MethodSource("testArguments")
+    void checkIfReservationIsExpired(
+            Date currentDate,
+            Date reservationDate,
+            DaysOfTheWeek daysOfTheWeekPolicy,
+            boolean expired) {
+        given(dateProvider.now()).willReturn(currentDate);
         var policy = ReservationExpirationPolicy.builder()
-                .timeLimit(limit)
-                .daysOfTheWeek(DaysOfTheWeek.ALL_DAYS)
+                .timeLimit(TIME_LIMIT)
                 .timeUnit(TimeUnit.DAYS)
+                .daysOfTheWeek(daysOfTheWeekPolicy)
                 .dateProvider(dateProvider).build();
 
-        var isExpired = policy.isExpired(DATE_13_06_2022);
+        var isExpired = policy.isExpired(reservationDate);
 
-        assertThat(isExpired).isFalse();
+        assertThat(isExpired).isEqualTo(expired);
     }
 
-    @Test
-    void checkIfReservationIsExpiredWhenWeekdaysPolicyAndCurrentTimeAfterExpiration() {
-        given(dateProvider.now()).willReturn(DATE_17_06_2022);
-        var policy = ReservationExpirationPolicy.builder()
-                .timeLimit(limit)
-                .daysOfTheWeek(DaysOfTheWeek.ALL_DAYS)
-                .timeUnit(TimeUnit.DAYS)
-                .dateProvider(dateProvider).build();
-
-        var isExpired = policy.isExpired(DATE_13_06_2022);
-
-        assertThat(isExpired).isTrue();
-    }
-
-
-    @Test
-    void checkIfReservationIsExpiredWhenWorkdaysPolicyAndCurrentTimeBeforeExpiration() {
-        given(dateProvider.now()).willReturn(DATE_20_06_2022);
-        var policy = ReservationExpirationPolicy.builder()
-                .timeLimit(limit)
-                .daysOfTheWeek(DaysOfTheWeek.WORKDAYS)
-                .timeUnit(TimeUnit.DAYS)
-                .dateProvider(dateProvider).build();
-
-        var isExpired = policy.isExpired(DATE_15_06_2022);
-
-        assertThat(isExpired).isFalse();
-    }
-
-    @Test
-    void checkIfReservationIsExpiredWhenWorkdaysPolicyAndCurrentTimeAfterExpiration() {
-        given(dateProvider.now()).willReturn(DATE_21_06_2022);
-        var policy = ReservationExpirationPolicy.builder()
-                .timeLimit(limit)
-                .daysOfTheWeek(DaysOfTheWeek.WORKDAYS)
-                .timeUnit(TimeUnit.DAYS)
-                .dateProvider(dateProvider).build();
-
-        var isExpired = policy.isExpired(DATE_15_06_2022);
-
-        assertThat(isExpired).isTrue();
+    static Stream<Arguments> testArguments() {
+        return Stream.of(
+                Arguments.of(DATE_15_06_2022, DATE_13_06_2022, DaysOfTheWeek.ALL_DAYS, false),
+                Arguments.of(DATE_17_06_2022, DATE_13_06_2022, DaysOfTheWeek.ALL_DAYS, true),
+                Arguments.of(DATE_20_06_2022, DATE_15_06_2022, DaysOfTheWeek.WORKDAYS, false),
+                Arguments.of(DATE_21_06_2022, DATE_15_06_2022, DaysOfTheWeek.WORKDAYS, true)
+        );
     }
 }
--- src/test/java/com/productdock/library/rental/util/FutureDateShould.java
+++ src/test/java/com/productdock/library/rental/util/FutureDateShould.java
@@ -1,10 +1,13 @@
 package com.productdock.library.rental.util;
 
 import com.productdock.library.rental.scheduled.DaysOfTheWeek;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,19 +17,19 @@ class FutureDateShould {
     private static final Date SATURDAY = new Date(1655632800000L);
     private static final Date TUESDAY = new Date(1655805600000L);
 
-    @Test
-    void addDurationToDateWhenWorkdayPolicy() {
+    @ParameterizedTest
+    @MethodSource("testArguments")
+    void addDurationToDate(DaysOfTheWeek daysOfTheWeekPolicy, Date date, Date expectedFutureDate) {
         var duration = new Duration(4, TimeUnit.DAYS);
-        var futureDate = FutureDate.of(WEDNESDAY, DaysOfTheWeek.WORKDAYS).offset(duration);
+        var futureDate = FutureDate.of(date, daysOfTheWeekPolicy).offset(duration);
 
-        assertThat(futureDate).isEqualTo(TUESDAY);
+        assertThat(futureDate).isEqualTo(expectedFutureDate);
     }
 
-    @Test
-    void addDurationToDateWhenWeekdayPolicy() {
-        var duration = new Duration(4, TimeUnit.DAYS);
-        var futureDate = FutureDate.of(WEDNESDAY, DaysOfTheWeek.ALL_DAYS).offset(duration);
-
-        assertThat(futureDate).isEqualTo(SATURDAY);
+    static Stream<Arguments> testArguments() {
+        return Stream.of(
+                Arguments.of(DaysOfTheWeek.WORKDAYS, WEDNESDAY, TUESDAY),
+                Arguments.of(DaysOfTheWeek.ALL_DAYS, WEDNESDAY, SATURDAY)
+                );
     }
 }
