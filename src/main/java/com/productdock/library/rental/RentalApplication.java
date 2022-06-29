a42c80bb2c2ecda813f9ba073d6caa0f24e50993
Milica Zivkov <milica.zivkov@productdock.com>
2022-06-29 10:04:10 +0200

    @SpringBootApplication already includes EnableAutoConfiguration

--- src/main/java/com/productdock/library/rental/RentalApplication.java
+++ src/main/java/com/productdock/library/rental/RentalApplication.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafka
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableMongoRepositories
 @EnableMongock
 @EnableScheduling
